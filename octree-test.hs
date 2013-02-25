2799ed41ab15422b29822aea6b74e0674e12843b
startling <tdixon51793@gmail.com>
2013-02-25 11:17:35 -0500

    adapted the lens law tests to 'path' and 'child'

--- octree-test.hs
+++ octree-test.hs
@@ -31,10 +31,29 @@ instance Arbitrary a => Arbitrary (Operation a) where
 -- Tell whether an octree is simplified.
 simplified :: Eq a => Octree a -> Bool
 simplified (Leaf _) = True
-simplified (Branch a b c d e f g h) = any (/= a) (tail l)
-  && all simplified l where l = [a, b, c, d, e, f, g, h]
+simplified (Branch (Leaf a) b c d e f g h) = any (/= Leaf a) l
+  && all simplified l where l = [b, c, d, e, f, g, h]
+simplified (Branch a b c d e f g h) = all simplified l
+  where l = [a, b, c, d, e, f, g, h]
 
 main = hspec $ do
+  describe "child" $ do
+    it "always gives a value for leaves" . property $
+      \os v -> Leaf (v :: Int) ^. child os == Leaf v
+    it "follows the first lens law" . property $
+      \os v t -> (t & child os .~ v)^.child os == (v :: Octree Int)
+    it "follows the second lens law" . property $ do
+      -- Only guaranteed for simplified trees.
+      t <- arbitrary `suchThat` simplified :: Gen (Octree Int)
+      os <- arbitrary
+      let r = t ^. child os
+      return $ (t & child os .~ r) == t
+    it "follows the third lens law" . property $ do
+      -- Only guaranteed for simplified trees.
+       t <- arbitrary `suchThat` simplified :: Gen (Octree Int)
+       (os, v1, v2) <- (,,) <$> arbitrary <*> arbitrary <*> arbitrary
+       return $ (t & child os .~ v1 & child os .~ v2)
+         == (t & child os .~ v2)
   describe "path" $ do
     it "creates a simplified tree when inserting" . property $
      \os v y -> simplified $ set (path os) (v :: Int) (Leaf y)
@@ -42,3 +61,18 @@ main = hspec $ do
       \os v -> Leaf (v :: Int) ^. path os == [v]
     it "follows the first lens law" . property $
       \os v t -> (t & path os .~ v)^.path os == [v :: Int]
+    it "follows the second lens law" . property $ do
+      -- Only guaranteed for simplified trees.
+      t <- arbitrary `suchThat` simplified :: Gen (Octree Int)
+      os <- arbitrary
+      return $ case t ^. path os of
+        v : [] -> (t & path os .~ v) == (t :: Octree Int)
+        _ -> True
+    it "follows the third lens law" . property $ do 
+       -- Only guaranteed for simplified trees.
+       t <- arbitrary `suchThat` simplified :: Gen (Octree Int)
+       os <- arbitrary
+       v1 <- arbitrary :: Gen Int
+       v2 <- arbitrary :: Gen Int
+       return $ (t & path os .~ v1 & path os .~ v2)
+          == (t & path os .~ v2)
